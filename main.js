f74ceecdfbdbcf40b7617896731efc6490212c31
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-19 17:20:57 +0100

    conclusa Milestone 2, inseriti commenti per precedenti

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 /* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
 BONUS 1:
@@ -12,42 +8,65 @@ BONUS 2:
 Organizzare i singoli membri in card/schede */
 
 
+const outputHTML= document.getElementById("output")
+
+
+//Creo l'array con tutti gli oggetti-membri
+//MILESTONE 0
 const team = [
     {
-        nome : "Wayne Barnett",
-        ruolo : "Founder & CEO",
-        foto : "wayne-barnett-founder-ceo.jpg"
+        Nome : "Wayne Barnett",
+        Ruolo : "Founder & CEO",
+        Foto : "wayne-barnett-founder-ceo.jpg"
     },
     {
-        nome : "Angela Caroll",
-        ruolo : "Chief Editor",
-        foto : "angela-caroll-chief-editor.jpg"
+        Nome : "Angela Caroll",
+        Ruolo : "Chief Editor",
+        Foto : "angela-caroll-chief-editor.jpg"
     },
     {
-        nome : "Walter Gordon",
-        ruolo : "Office Manager",
-        foto : "walter-gordon-office-manager.jpg"
+        Nome : "Walter Gordon",
+        Ruolo : "Office Manager",
+        Foto : "walter-gordon-office-manager.jpg"
     },
     {
-        nome : "Angela Lopez",
-        ruolo : "Social Media Manager",
-        foto : "angela-lopez-social-media-manager.jpg"
+        Nome : "Angela Lopez",
+        Ruolo : "Social Media Manager",
+        Foto : "angela-lopez-social-media-manager.jpg"
     },
     {
-        nome : "Scott Estrada",
-        ruolo : "Developer",
-        foto : "scott-estrada-developer.jpg"
+        Nome : "Scott Estrada",
+        Ruolo : "Developer",
+        Foto : "scott-estrada-developer.jpg"
     },
     {
-        nome : "Barbara Ramos",
-        ruolo : "Graphic Designer",
-        foto : "barbara-ramos-graphic-designer.jpg"
+        Nome : "Barbara Ramos",
+        Ruolo : "Graphic Designer",
+        Foto : "barbara-ramos-graphic-designer.jpg"
     }
 ]
 
+//ciclo per ogni elemento dell'array tutte le chiavi e i relativi valori
+//MILESTONE 1
 for(let i=0; i<team.length; i++){
     for (let key in team[i]){
         console.log(`${key} : ${team[i][key]}`)
     }
     console.log("")
+}
+
+//MILESTONE 2
+for(let i=0; i<team.length; i++){
+    outputHTML.innerHTML+=stringaOut(team[i])
+}
+
+
+function stringaOut(object){
+    let stampa = ""
+    for (let key in object){
+        stampa += key + " : " + object[key] + " - "
+    }
+    stampa += "<br>"
+    console.log(stampa)
+    return stampa
 }
