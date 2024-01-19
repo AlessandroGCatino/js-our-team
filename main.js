ea7cd0ec57b111ce5fead135f61205626d657b47
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-19 16:46:26 +0100

    conclusione Milestone 0

--- main.js
+++ main.js
@@ -11,9 +11,36 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede */
 
-/*Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
-Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
-Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
-Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-Scott Estrada	Developer	scott-estrada-developer.jpg
-Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg*/
+
+const team = [
+    {
+        nome : "Wayne Barnett",
+        ruolo : "Founder & CEO",
+        foto : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome : "Angela Caroll",
+        ruolo : "Chief Editor",
+        foto : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome : "Walter Gordon",
+        ruolo : "Office Manager",
+        foto : "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome : "Angela Lopez",
+        ruolo : "Social Media Manager",
+        foto : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome : "Scott Estrada",
+        ruolo : "Developer",
+        foto : "scott-estrada-developer.jpg"
+    },
+    {
+        nome : "Barbara Ramos",
+        ruolo : "Graphic Designer",
+        foto : "barbara-ramos-graphic-designer.jpg"
+    }
+]
